b75251e2ff6abdc1c04b40eabdd35ab071d276fe
Adrien N. F <iamnautilusjungle@gmail.com>
2023-01-20 21:29:02 -0500

    export to html

--- src/services/Exporter/HtmlExporter.tsx
+++ src/services/Exporter/HtmlExporter.tsx
@@ -3,6 +3,15 @@ import { Exporter } from "./Exporter";
 
 export class HtmlExporter implements Exporter {
 	public export(reportData: ReportData): void {
-		navigator.clipboard.writeText("coucou toi");
+		const buffer: string[] = [];
+
+		reportData.projects.forEach(p => {
+			buffer.push(`<h1>${p.name}</h1>`);
+			buffer.push("<ul>");
+			buffer.push(p.commits.split("\n").map(e => `<li>${e}</li>`).join("\n"));
+			buffer.push("</ul>");
+		})
+
+		navigator.clipboard.writeText(buffer.join("\n"));
 	}
 }
