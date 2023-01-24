ee3a1566bca35458e1fa00c65246f99e24ebd93c
Adrien N. F <iamnautilusjungle@gmail.com>
2023-01-23 23:18:17 -0500

    updated exports

--- src/services/Exporter/HtmlExporter.tsx
+++ src/services/Exporter/HtmlExporter.tsx
@@ -5,12 +5,16 @@ export class HtmlExporter implements Exporter {
 	public export(reportData: ReportData): void {
 		const buffer: string[] = [];
 
+		buffer.push(`<p>${reportData.before}</p>`)
+
 		reportData.projects.forEach(p => {
 			buffer.push(`<h1>${p.name}</h1>`);
 			buffer.push("<ul>");
 			buffer.push(p.commits.split("\n").map(e => `<li>${e}</li>`).join(""));
 			buffer.push("</ul>");
 		})
+
+		buffer.push(`<p>${reportData.after}</p>`)
 
 		navigator.clipboard.writeText(buffer.join(""));
 	}
--- src/services/Exporter/TextExporter.tsx
+++ src/services/Exporter/TextExporter.tsx
@@ -5,10 +5,14 @@ export class TextExporter implements Exporter {
 	public export(reportData: ReportData): void {
 		const buffer: string[] = [];
 
+		buffer.push(reportData.before);
+
 		reportData.projects.forEach(p => {
 			buffer.push(p.name);
 			buffer.push(p.commits.split("\n").map(e => `- ${e}`).join("\n"));
 		})
+
+		buffer.push(reportData.after);
 
 		navigator.clipboard.writeText(buffer.join("\n"));
 	}
