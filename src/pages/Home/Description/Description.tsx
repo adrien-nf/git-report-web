8dda9ad515f0820db63b0ec85ea3f472d8f6364c
Adrien N. F <iamnautilusjungle@gmail.com>
2023-01-25 16:39:37 -0500

    fill empty space

--- src/pages/Home/Description/Description.tsx
+++ src/pages/Home/Description/Description.tsx
@@ -1,6 +1,7 @@
 import { Stack, Grid } from "@mui/material";
 import GitReportTitle from "../../../components/GitReportTitle/GitReportTitle";
 import SubTitle from "../SubTitle/SubTitle";
+import LogoMute from "../../../assets/LogoMute.svg";
 
 export default function Description() {
 	return (
@@ -9,6 +10,13 @@ export default function Description() {
 				<Grid item xs={12} md={6}>
 					<GitReportTitle />
 					<SubTitle>Introducing a new way to analyze and report your Git activity. With our command-line script, you can quickly generate detailed reports of your commits. Easily see what you worked on and how much time you spent on it!</SubTitle>
+				</Grid>
+				<Grid item display={{ xs: "none", "md": "block" }} md={6}>
+					<img src={LogoMute} style={{
+						userSelect: "none",
+						MozUserSelect: "none",
+						WebkitUserSelect: "none",
+					}} draggable="false" alt="Logo GitReport" />
 				</Grid>
 			</Grid>
 		</Stack>
