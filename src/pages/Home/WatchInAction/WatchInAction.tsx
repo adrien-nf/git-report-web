41a6ec21d1e83d051fa3cb746eff5bef03cc169c
Nathan Gobinet <nathan.gobinet@gmail.com>
2023-04-10 22:32:46 -0400

    Add demo Gif

--- src/pages/Home/WatchInAction/WatchInAction.tsx
+++ src/pages/Home/WatchInAction/WatchInAction.tsx
@@ -1,16 +1,16 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import demo from '../../../assets/demo.gif';
+import { styled } from "@mui/material";
+
+const ResponsiveGif = styled('img')({
+	border: '1px solid #00C2FF11',
+})
 
 export default function WatchInAction() {
 	return (
 		<section id="demo">
 			<SectionTitle>Watch it in action!</SectionTitle>
-			<iframe
-				width="100%"
-				src="https://www.youtube.com/embed/wnhvanMdx4s"
-				style={{ border: 0, aspectRatio: 16 / 9 }}
-				title="GitReport demo"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-				allowFullScreen></iframe>
+			<ResponsiveGif src={demo} width="100%" />
 		</section>
 	)
 }
